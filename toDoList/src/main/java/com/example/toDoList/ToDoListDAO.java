d5fd4b673b42def7eb6afa81a798d138d3876eb5
MihashiMazki <hisyamudin1708@gmail.com>
2024-12-12 02:04:37 +0800

    Update ToDoListDAO.java
    Add CreateToDoList method in the file. (There are difference between the uncommented and the commented function where the first one, the id is not returned whereas the other fx returned the id). You can choose according to your liking.

--- toDoList/src/main/java/com/example/toDoList/ToDoListDAO.java
+++ toDoList/src/main/java/com/example/toDoList/ToDoListDAO.java
@@ -24,4 +24,45 @@ public class ToDoListDAO {
         });
     }
 
+    ////// NO ID IS RETURNED
+
+    public ToDoListModel createToDoList(ToDoListModel model) {
+
+        String sql = "INSERT INTO ToDoList (task_name, task_desc, status) VALUES (?, ?, ?)";
+    
+        int rowsAffected = jdbcTemplate.update(sql, model.getTaskName(), model.getTaskDesc(), model.getStatus());
+    
+        if (rowsAffected > 0) {
+            return model;
+        } else {
+            return null;
+        }
+    }
+
+    //////////// IF NEED TO RETURN THE ID AFTER BEING CREATED (POST)//////////////
+
+    // public ToDoListModel createToDoList(ToDoListModel model){
+
+    //     String sql = "INSERT INTO ToDoList (task_name, task_desc, status) VALUES (?, ?, ?)";
+
+    //     KeyHolder keyHolder = new GeneratedKeyHolder();
+    
+    //     int rowsAffected = jdbcTemplate.update(connection -> {
+    //         PreparedStatement ps = connection.prepareStatement(sql, new String[]{"no"});
+    //         ps.setString(1, model.getTaskName());
+    //         ps.setString(2, model.getTaskDesc());
+    //         ps.setString(3, model.getStatus());
+    //         return ps;
+    //     }, keyHolder);
+
+    //     if (rowsAffected > 0) {
+    //         // Retrieve the generated 'no' and set it in the model
+    //         model.setNo(keyHolder.getKey().intValue());
+    //         return model;
+    //     } else {
+    //         return null;
+    //     }
+
+    // }
+
 }
